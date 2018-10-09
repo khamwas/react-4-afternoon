e193c4a2d0bbe9123958f94788f039d22ac30412
Emmanuel Bravo <emmanuel@Emmanuels-MacBook-Pro.local>
2018-10-09 15:22:02 -0500

    completed with black diamonds

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default class ClassList extends Component {
   constructor() {
-    super()
-    
+    super();
+    this.state = {
+      students: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `http://localhost:3005/students?class=${this.props.match.params.class}`
+      )
+      .then(results => this.setState({ students: results.data }));
   }
 
   render() {
+    let mapped = this.state.students.map(elem => (
+      <Link key={elem.id} to={`/student/${elem.id}`}>
+        <h3>
+          {elem.first_name} {elem.last_name} {elem.id}
+        </h3>
+      </Link>
+    ));
     return (
-      <div className="box">
-        <h1></h1>
-        <h2>ClassList:</h2>
-
+      <div>
+        <div className="box">
+          <h1>{this.props.match.params.class}</h1>
+          <h2>ClassList:</h2>
+          {mapped}
+        </div>
+        <Link to="/">
+          <button>Back</button>
+        </Link>
       </div>
-    )
+    );
   }
 }
